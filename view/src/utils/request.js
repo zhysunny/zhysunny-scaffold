de417b7104366292b8e36ced5262818c61487473
zhysunny <1065300897@qq.com>
2020-12-20 17:57:27 +0800

    http请求设置

--- view/src/utils/request.js
+++ view/src/utils/request.js
@@ -6,8 +6,11 @@ import { getToken } from '@/utils/auth'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000, // request timeout
+  headers: {
+    'Content-type': 'application/json;charset=UTF-8'
+  }
 })
 
 // request interceptor
